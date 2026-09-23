74b20f57e7dadc1dc6d628ef374860e6d000f5bc
Raj Chakrawarty <rajchakrawarty9999-sketch@users.noreply.github.com>
2026-09-23 20:37:36 +0530

    Add Java beginner programs

---
+++ FirstProgram/PrintingNumber.java
@@ -0,0 +1,11 @@
+package FirstProgram;
+
+public class PrintingNumber {
+    public static void main(String[] args){
+        System.out.println("4");
+        System.out.println(4+5);
+        System.out.println(6-2); 
+        System.out.println("4+5");
+    }
+    
+}
---
+++ FirstProgram/VariableInt.java
@@ -0,0 +1,11 @@
+public class VariableInt {
+    public static void main(String[] args){
+        int x,y;
+        x = 10;
+        y = 11;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x+y+1);
+    }
+    
+}
